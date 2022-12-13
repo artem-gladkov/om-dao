501237b2818b8caf60042f5d197d1ce9198feba9
mitagmio <mitag.mio@gmail.com>
2022-12-13 12:20:22 +0000

    show Tigr hide CR

--- src/features/show-projects/ui/Projects.tsx
+++ src/features/show-projects/ui/Projects.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 import { Project } from "./Project";
 
-// import TigrImg from "../../../app/images/tokens/omdwtigr.webp";
-import CRImg from "../../../app/images/tokens/omdwcrb.webp";
+import TigrImg from "../../../app/images/tokens/omdwtigr.webp";
+// import CRImg from "../../../app/images/tokens/omdwcrb.webp";
+// import CONSImg from "../../../app/images/tokens/omdwcons.webp";
 import { TOKEN_SYMBOLS } from "../../../entities";
 
 export interface IProjectsProps {}
@@ -10,8 +11,9 @@ export interface IProjectsProps {}
 export const Projects: FC<IProjectsProps> = () => {
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      {/* <Project symbol={TOKEN_SYMBOLS.TIGR} img={TigrImg} /> */}
-      <Project symbol={TOKEN_SYMBOLS.CR} img={CRImg} />
+      <Project symbol={TOKEN_SYMBOLS.TIGR} img={TigrImg} />
+      {/* <Project symbol={TOKEN_SYMBOLS.CR} img={CRImg} /> */}
+      {/* <Project symbol={TOKEN_SYMBOLS.CONS} img={CONSImg} /> */}
     </div>
   );
 };
