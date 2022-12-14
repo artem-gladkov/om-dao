bb508c3ece0f4799d70689f07f240727f42d9384
mitagmio <mitag.mio@gmail.com>
2022-12-14 09:05:41 +0000

    show cons

--- src/features/show-projects/ui/Projects.tsx
+++ src/features/show-projects/ui/Projects.tsx
@@ -3,7 +3,7 @@ import { Project } from "./Project";
 
 import TigrImg from "../../../app/images/tokens/omdwtigr.webp";
 // import CRImg from "../../../app/images/tokens/omdwcrb.webp";
-// import CONSImg from "../../../app/images/tokens/omdwcons.webp";
+import CONSImg from "../../../app/images/tokens/omdwcons.webp";
 import { TOKEN_SYMBOLS } from "../../../entities";
 
 export interface IProjectsProps {}
@@ -13,7 +13,7 @@ export const Projects: FC<IProjectsProps> = () => {
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
       <Project symbol={TOKEN_SYMBOLS.TIGR} img={TigrImg} />
       {/* <Project symbol={TOKEN_SYMBOLS.CR} img={CRImg} /> */}
-      {/* <Project symbol={TOKEN_SYMBOLS.CONS} img={CONSImg} /> */}
+      <Project symbol={TOKEN_SYMBOLS.CONS} img={CONSImg} />
     </div>
   );
 };
