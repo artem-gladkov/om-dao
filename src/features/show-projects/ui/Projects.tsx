59fc053c6008821140988f72c0d41548ae2efeea
mitagmio <mitag.mio@gmail.com>
2023-01-15 03:18:12 +0000

    hide CR

--- src/features/show-projects/ui/Projects.tsx
+++ src/features/show-projects/ui/Projects.tsx
@@ -14,7 +14,7 @@ export const Projects: FC<IProjectsProps> = () => {
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
       <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.TIGR]} symbol={TOKEN_SYMBOLS.TIGR} img={TigrImg} href={TOKEN_HREF[TOKEN_SYMBOLS.TIGR]} />
       {/* <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.CONS]} symbol={TOKEN_SYMBOLS.CONS} img={ConsImg} href={TOKEN_HREF[TOKEN_SYMBOLS.CONS]}/> */}
-      <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.CR]} symbol={TOKEN_SYMBOLS.CR} img={CRImg} href={TOKEN_HREF[TOKEN_SYMBOLS.CR]}/>
+      {/* <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.CR]} symbol={TOKEN_SYMBOLS.CR} img={CRImg} href={TOKEN_HREF[TOKEN_SYMBOLS.CR]}/> */}
     </div>
   );
 };
