c5a548bec0a68e3520fa6a48bbb8d5177046e9e9
AlexDv19 <alexdvinvest@list.ru>
2023-01-19 21:01:07 +0300

    add omdwLed

--- src/features/show-projects/ui/Projects.tsx
+++ src/features/show-projects/ui/Projects.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Project } from "./Project";
 
 import TigrImg from "../../../app/images/tokens/omdwtigr.webp";
+import LedImg from "../../../app/images/tokens/omdwled.webp";
 
 // import ConsImg from "../../../app/images/tokens/omdwcons.webp";
 // import CRImg from "../../../app/images/tokens/omdwcrb.webp";
@@ -13,6 +14,8 @@ export const Projects: FC<IProjectsProps> = () => {
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
       <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.TIGR]} symbol={TOKEN_SYMBOLS.TIGR} img={TigrImg} href={TOKEN_HREF[TOKEN_SYMBOLS.TIGR]} />
+      <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.LED]} symbol={TOKEN_SYMBOLS.LED} img={LedImg} href={TOKEN_HREF[TOKEN_SYMBOLS.LED]} />
+
       {/* <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.CONS]} symbol={TOKEN_SYMBOLS.CONS} img={ConsImg} href={TOKEN_HREF[TOKEN_SYMBOLS.CONS]}/> */}
       {/* <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.CR]} symbol={TOKEN_SYMBOLS.CR} img={CRImg} href={TOKEN_HREF[TOKEN_SYMBOLS.CR]}/> */}
     </div>
