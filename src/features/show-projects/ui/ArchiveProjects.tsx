575bed9013562d1922a55f60d262b38f31890232
artemglvdkov <av.gladkov@interfax.ru>
2023-01-21 18:13:51 +0300

    fix: fix conflicts

--- src/features/show-projects/ui/ArchiveProjects.tsx
+++ src/features/show-projects/ui/ArchiveProjects.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react";
 import { Project } from "./Project";
 
-import TigrImg from "../../../app/images/tokens/omdwtigr.webp";
 import { TOKEN_SYMBOLS, TOKEN_HREF, TOKEN_TITLE } from "../../../entities";
 
 
 export const ArchiveProjects: FC = () => {
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.TIGR]} symbol={TOKEN_SYMBOLS.TIGR} img={TigrImg} href={TOKEN_HREF[TOKEN_SYMBOLS.TIGR]} />
+      <Project title={TOKEN_TITLE[TOKEN_SYMBOLS.TIGR]} symbol={TOKEN_SYMBOLS.TIGR} href={TOKEN_HREF[TOKEN_SYMBOLS.TIGR]} />
     </div>
   );
 };
