760a8d6ce755f53c655dcc2af5e6a0bb06f5bbad
mitagmio <mitag.mio@gmail.com>
2023-02-02 13:41:53 +0000

    move to active CONT

--- src/features/show-projects/ui/WaitingProjects.tsx
+++ src/features/show-projects/ui/WaitingProjects.tsx
@@ -9,7 +9,6 @@ export const WaitingProjects: FC = () => {
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
       <Project buttonName={buttonName} title={TOKEN_TITLE[TOKEN_SYMBOLS.CR]} symbol={TOKEN_SYMBOLS.CR} href={TOKEN_HREF[TOKEN_SYMBOLS.CR]} />
       <Project buttonName={buttonName} title={TOKEN_TITLE[TOKEN_SYMBOLS.CONS]} symbol={TOKEN_SYMBOLS.CONS} href={TOKEN_HREF[TOKEN_SYMBOLS.CONS]} />
-      <Project buttonName={buttonName} title={TOKEN_TITLE[TOKEN_SYMBOLS.CONT]} symbol={TOKEN_SYMBOLS.CONT} href={TOKEN_HREF[TOKEN_SYMBOLS.CONT]} />
       <Project buttonName={buttonName} title={TOKEN_TITLE[TOKEN_SYMBOLS.CHAI]} symbol={TOKEN_SYMBOLS.CHAI} href={TOKEN_HREF[TOKEN_SYMBOLS.CHAI]} />
     </div>
   );
