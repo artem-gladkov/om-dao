c928570ac4f49dcc42c077d77eb5d2b2314ae1f4
Artem Gladkov <artem.gladkovw@gmail.com>
2023-06-25 11:40:06 +0300

    feat: Добавлено сохранение языка в localstorage

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,6 +1,8 @@
 import i18n, { InitOptions } from "i18next";
 import { initReactI18next } from "react-i18next";
 import { resources } from "./resources"
+import LanguageDetector from "i18next-browser-languagedetector"
+import Backend from "i18next-http-backend"
 
 const DEFAULT_I_18_N_OPTIONS:InitOptions = {
     resources,
@@ -10,12 +12,19 @@ const DEFAULT_I_18_N_OPTIONS:InitOptions = {
     },
     react: {
         useSuspense: false
-    }
+    },
+    detection: {
+        order: ["localStorage"],
+        lookupLocalStorage: "__locale__",
+        caches: ["localStorage"],
+    },
 }
 export const createI18n = (customOptions?: InitOptions ) => {
     const options:InitOptions = {...DEFAULT_I_18_N_OPTIONS, ...customOptions}
     const instance = i18n.createInstance(options)
 
+    instance.use(Backend)
+    instance.use(LanguageDetector)
     instance.use(initReactI18next)
     instance.init()
 
